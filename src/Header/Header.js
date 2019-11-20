54739ccfa42089e061d693cf4a4ea9297c0d3a04
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-11-20 21:56:06 +0200

    Update header styling, wip

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div className="content content--fixed">
+    <div className="content top-header">
       <header className="codrops-header">
         <h1 className="codrops-header__title">Holy Molly</h1>
       </header>
