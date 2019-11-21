e8d02e375ef75be71dc776aecebe824ec237f2eb
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-11-21 22:09:35 +0200

    clean loading

--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -2,13 +2,7 @@ import './Loading.css';
 
 export default function Loading(props) {
   return (
-    // <div className="loading-overlay" >
-    //   <div className='center'>
-    //     <div className='loader'>
-    //       <div class="loader">Loading...</div>
-    //     </div>
-    //   </div>
-    // </div>
+   
     null
   )
 }
