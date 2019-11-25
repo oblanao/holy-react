fc304c179cc89e89808cd2f927b3e40efc014529
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-11-25 18:20:06 +0200

    Add subtitle to disco page

--- src/Pages/Discography/Discography.js
+++ src/Pages/Discography/Discography.js
@@ -6,9 +6,9 @@ export default function Discography() {
   return (
     <div className="content__inner">
       <h2 className="content__title">discography</h2>
-      {/* <h3 className="content__subtitle">
-        Coming soon ...
-          </h3> */}
+      <h3 className="content__subtitle">
+        Listen to my latest hits
+          </h3>
       <div className="content__rest">
         <iframe src="https://open.spotify.com/embed/track/6p6qMdoReQ6tfBgidP4S4x" width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
