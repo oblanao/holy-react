d83993c51dd0ed921f2589844601c8f0ca0d97ea
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-12-09 11:01:05 +0200

    Finish About page

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -5,15 +5,15 @@ export default function AboutMe() {
   return (
     <div className="content__inner">
       <h2 className="content__title">about me</h2>
-      <h3 className="content__subtitle">
+      <h3 className="content__subtitle lower">
         Chameleonic artist that perfectly blends with her vocals in different music
         genres such as pop, trap, R&B, electro, soul, rock, deephouse
       </h3>
-      <div className="content__rest">
+      {/* <div className="content__rest">
         <p>Haules baules</p>
         <p>lorem ipsum dolor sit amet et conescietur</p>
         <img src="https://via.placeholder.com/350x200.png" alt="asd" />
-      </div>      
+      </div>       */}
     </div>
   )
 }
