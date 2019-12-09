b439eab9b8a17fd8b04cf520b1297b22570b93b3
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-12-09 11:24:29 +0200

    Finish Philosophy page

--- src/Pages/Philosophy/Philosophy.js
+++ src/Pages/Philosophy/Philosophy.js
@@ -5,8 +5,19 @@ export default function Philosophy() {
     <div className="content__inner">
       <h2 className="content__title">Philosophy</h2>
       <h3 className="content__subtitle">
-        Holy Molly Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores cupiditate rem ipsam expedita blanditiis ea ex officiis magnam, ducimus doloremque deleniti. Commodi, voluptate ipsam! Blanditiis, modi. Sed quasi officiis nulla.
       </h3>
+      <div className="content__rest philosophy-content">
+        <p>
+          I started my musical journey in November 2019 with the song Everybody's Scared, an emotional metaphor of what I've been going through in the past years.
+        </p>
+        <p>
+          As a musician and creator, I live to express and spread intense feelings inside my listeners' hearts.
+          I've gone through different stages of anxiety, depression and frustration while trying to build and understand who I am as an artist.
+        </p>
+        <p>
+          Now I am putting it all on the table and I won't stop until you know my name
+        </p>
+      </div>
     </div>
   )
 }
