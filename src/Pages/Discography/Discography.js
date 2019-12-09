8e8179bf4a4eb87109c6cc6fd7a39349ff242472
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2019-12-09 10:56:30 +0200

    Finish Disco page

--- src/Pages/Discography/Discography.js
+++ src/Pages/Discography/Discography.js
@@ -6,20 +6,20 @@ export default function Discography() {
   return (
     <div className="content__inner">
       <h2 className="content__title">discography</h2>
-      <h3 className="content__subtitle">
+      <h3 className="content__subtitle hide-mobile">
         Listen to my latest hits
           </h3>
       <div className="content__rest">
         <ul className="songs-list">
           <li className="sketchy">
-            <iframe title="firstSong" src="https://open.spotify.com/embed/track/6p6qMdoReQ6tfBgidP4S4x" width="300" height="78" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe title="Everybody's Scared" src="https://open.spotify.com/embed/track/58WVilSugrjanFOTpVtq8d" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           </li>
-          <li className="sketchy">
+          {/* <li className="sketchy">
             <iframe title="secondSong" src="https://open.spotify.com/embed/track/6p6qMdoReQ6tfBgidP4S4x" width="300" height="78" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           </li>
           <li className="sketchy">
             <iframe title="thirdSong" src="https://open.spotify.com/embed/track/6p6qMdoReQ6tfBgidP4S4x" width="300" height="78" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
