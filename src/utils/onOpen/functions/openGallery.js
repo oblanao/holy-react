079b6e690fdf09ed4fd10cddbc417840ac6e9646
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2020-08-22 16:51:40 +0300

    remove module.exports

--- src/utils/onOpen/functions/openGallery.js
+++ src/utils/onOpen/functions/openGallery.js
@@ -1,32 +1,36 @@
-const axios = require('axios');
+import axios from "axios";
 
 const refreshMedia = async () => {
   try {
-    const htmlString = await axios.get('https://www.instagram.com/holy.molly.guacamolly')
+    const htmlString = await axios.get(
+      "https://www.instagram.com/holy.molly.guacamolly"
+    );
     let parser = new DOMParser();
-    const htmlDom = parser.parseFromString(htmlString.data, 'text/html');
-    const scriptArr = htmlDom.querySelectorAll('script');
+    const htmlDom = parser.parseFromString(htmlString.data, "text/html");
+    const scriptArr = htmlDom.querySelectorAll("script");
     const mediaStr = scriptArr[4].innerText;
-    const mediaJson = JSON.parse(mediaStr.substr(21, mediaStr.length - 22))
-    const mediaArr = mediaJson.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
-    localStorage.setItem('ig_media', JSON.stringify(mediaArr));
+    const mediaJson = JSON.parse(mediaStr.substr(21, mediaStr.length - 22));
+    const mediaArr =
+      mediaJson.entry_data.ProfilePage[0].graphql.user
+        .edge_owner_to_timeline_media.edges;
+    localStorage.setItem("ig_media", JSON.stringify(mediaArr));
     const expDate = new Date().getTime() + 1800000;
-    localStorage.setItem('ig_expDate', expDate)
+    localStorage.setItem("ig_expDate", expDate);
     return mediaArr;
   } catch (err) {
-    alert('Unexpected error fetching IG');
+    alert("Unexpected error fetching IG");
     window.location.reload(true);
   }
-}
+};
 
 const openGallery = async () => {
-  localStorage.setItem('igMedia_ready', false);
+  localStorage.setItem("igMedia_ready", false);
   const NOW = new Date().getTime();
-  const expDate = localStorage.getItem('ig_expDate') || 0;
+  const expDate = localStorage.getItem("ig_expDate") || 0;
   if (expDate < NOW) {
     await refreshMedia();
   }
-  localStorage.setItem('igMedia_ready', true)
-}
+  localStorage.setItem("igMedia_ready", true);
+};
 
-module.exports = { openGallery };
+export { openGallery };
